bc788a4617b768020f0ef2ec5194cf208d671f95
Mishchenko Artem <The.Nesh@yandex.ru>
2024-11-20 15:51:58 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -5,38 +5,77 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+    // --- Состояние
     const [form, setForm] = useState([]);
+
+    // --- Слушатель формы
     const handleSubmit = (evt) => {
         evt.preventDefault();
         const { target } = evt;
         const formData = new FormData(target);
         const data = Object.fromEntries(formData);
         if (data.date === '' || data.distance === '') return;
-        setForm([
-            {
-                date: data.date,
-                distance: data.distance,
-                id: Math.random(),
-            },
-            ...form,
-        ]);
+        if (form.length === 0) {
+            createData(data);
+        } else if (form.find((elem) => elem.date === data.date)) {
+            updateData(data);
+        } else {
+            createData(data);
+        }
+
         target.date.value = '';
         target.distance.value = '';
-        console.log('handleSubmit', form);
     };
 
+    // --- Добавление данных в состояние
+    const createData = (data) => {
+        const newForm = () => {
+            return [
+                {
+                    date: data.date,
+                    distance: data.distance,
+                    id: Math.random()
+                },
+                ...form,
+            ];
+        };
+        setForm(newForm);
+    };
+
+    // --- Обновление данных в состоянии
+    const updateData = (data) => {
+        const formUpdate = form.map((elem, index) => {
+            if (elem.date === data.date) {
+                return {
+                    ...elem,
+                    distance: Number(elem.distance) + Number(data.distance),
+                };
+            } else {
+                return elem;
+            }
+        });
+        setForm(formUpdate);
+    };
+
+    // --- Сортировка данных в состоянии
     const sortData = form.sort((a, b) => {
         return new Date(b.date) - new Date(a.date);
     });
 
+    // --- Удаление элемента из состояния
     const handleDelete = (e) => {
         const parentTarget = e.target.closest('.list__wrapper-items');
-        console.log('handleDelete', parentTarget);
-        setForm([parentTarget.remove(), ...form]);
+        setForm((prevState) => {
+            return prevState.filter(
+                (elem) => elem.id !== Number(parentTarget.id),
+            );
+        });
     };
 
+    // --- Отображения списка состояния
     const renderTicket = sortData.map((item) => {
         if (typeof item === 'undefined') return;
+
         return (
             <div className="list__wrapper-items" key={item.id} id={item.id}>
                 <li>{item.date}</li>
@@ -53,10 +92,12 @@ function App() {
         );
     });
 
+    // --- Эффект клика по кнопке
     const onMouseDown = (e) => {
         e.target.style.transform = 'scale(.8)';
     };
 
+    // --- Эффект клика по кнопке
     const onMouseUp = (e) => {
         e.target.style.transform = 'scale(1)';
     };
@@ -81,7 +122,7 @@ function App() {
             <div>
                 <ul className="list">
                     <div className="list__wrapper-title">
-                        <p>Дата(ДД.ММ.ГГ)</p>
+                        <p>Дата &nbsp; (ДД.ММ.ГГ)</p>
                         <p>Пройдено км</p>
                         <p>Действия</p>
                     </div>
